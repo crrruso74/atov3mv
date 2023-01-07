f0e5b7b8140f32443683f5e61cbde92586deaf4d
sky-creations <121612750+sky-botz@users.noreply.github.com>
2023-01-07 17:14:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID', 24281454])
-API_HASH = environ['API_HASH', 'dbe4521b4291da85becb65c7d4d4c36c']
-BOT_TOKEN = environ['BOT_TOKEN', '5950974905:AAEaz0PFVTgclkKCJALlUrfaZhcqfKKmgVc']
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://user:user@cluster0.29yokea.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "custom")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
